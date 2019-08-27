fa2498d785e73af7cca75f134232148bb6ec57ac
KevinLo <wind19891002@gmail.com>
2019-08-27 20:54:08 +0800

    Version 0.0

---
+++ RideTogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RideTogether
+//
+//  Created by 戴汝羽 on 2019/8/27.
+//  Copyright © 2019 KevinKLLo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
