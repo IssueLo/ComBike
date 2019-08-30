f48213092d2d2affb4351bcb1d8355757b5064f1
KevinLo <wind19891002@gmail.com>
2019-08-30 14:12:19 +0800

    Add SingInView, UserLoginInController

---
+++ RideTogether/View/UserSignInView.swift
@@ -0,0 +1,28 @@
+//
+//  UserSignInView.swift
+//  RideTogether
+//
+//  Created by 戴汝羽 on 2019/8/30.
+//  Copyright © 2019 KevinKLLo. All rights reserved.
+//
+
+import UIKit
+
+class UserSignInView: UIView {
+    
+    @IBOutlet weak var userEmailLabel: UITextField!
+    
+    @IBOutlet weak var userPasswordLabel: UITextField!
+    
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    @IBAction func confirmSignUp() {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+        
+        
+}
