15799bd1fc28cbf46bff9de4c03eed50eb2a9035
KevinLo <wind19891002@gmail.com>
2019-10-04 14:40:42 +0800

    Add GroupSortingManager

---
+++ RideTogether/Model/GroupSortingManager.swift
@@ -0,0 +1,9 @@
+//
+//  GroupSortingManager.swift
+//  RideTogether
+//
+//  Created by 戴汝羽 on 2019/10/4.
+//  Copyright © 2019 KevinKLLo. All rights reserved.
+//
+
+import Foundation
---
+++ RideTogether/View/RidingGroup/GroupListHeader/GroupListHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  GroupListHeaderView.swift
+//  RideTogether
+//
+//  Created by 戴汝羽 on 2019/10/4.
+//  Copyright © 2019 KevinKLLo. All rights reserved.
+//
+
+import UIKit
+
+class GroupListHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
